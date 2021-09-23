190703cbf68bc0710a62e02d08e1986060bece9a
GitHub Action <action@github.com>
2021-09-23 09:42:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/wuhang/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wuhang/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wuhang.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wuhang.template.MyBundle
 
 class MyApplicationService {
 
